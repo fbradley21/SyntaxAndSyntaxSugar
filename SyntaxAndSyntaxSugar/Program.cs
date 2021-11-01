31803190ed1f56a177c3e5079707230f4bac39d3
Frederick Bradley <fbradley21@icloud.com>
2021-11-01 00:51:49 -0500

    completed Syntax exercise

---
+++ SyntaxAndSyntaxSugar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SyntaxAndSyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+
+
+
+            if (answer < 9)
+            {
+                Console.WriteLine($"{answer} is less than 9");
+            }
+            else
+            {
+                Console.WriteLine($"{answer} is correct!");
+            }
+
+        }
+    }
+}
